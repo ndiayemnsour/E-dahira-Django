c1791e2018ee1348bc0accbea044195c751da077
ndiayemnsour <zocod3r@gmail.com>
2025-04-27 21:44:16 +0000

    creation du serializers

---
+++ Edahiras/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+from .models import Membres, Dahiras, Audio, Localites, Sections
+
+#le serializer sert à convertir le modele en JSON
+class MemberSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Membres
+        fields = "__all__"
+        read_only_fields = ("id", "date_inscription")
+
+class DahirasSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Dahiras
+        fields = "__all__"
+        read_only_fields = ("id", "date_creation")
+
+class AudioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Audio
+        fields = "__all__"
+        read_only_fields = ("id", "date_audio")
+
+class LocalitesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Localites
+        fields = "__all__"
+        read_only_fields = ("id")
+
+class SectionsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Sections
+        fields = "__all__"
+        read_only_fields = ("id")
